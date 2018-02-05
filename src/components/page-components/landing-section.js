8d07fc341528f6690ba8f2fadf0ff52cef865236
Sebastian Bastidas <sbastidasr@gmail.com>
2018-02-05 18:10:17 -0500

    refactor

--- src/components/page-components/landing-section.js
+++ src/components/page-components/landing-section.js
@@ -1,8 +1,5 @@
 import React from "react";
-
 import "./landing-section.css";
-
-const InnerHTML = require('dangerously-set-inner-html')
 
 export default class LandingSection extends React.Component {
   render() {
@@ -15,53 +12,26 @@ export default class LandingSection extends React.Component {
     if (this.props.show === "right") {
       textAlign = "left-section";
       imageAlign = "right-section";
+      sectionBg = "grey-bg";
     } else {
       textAlign = "right-section";
       imageAlign = "left-section";
-    }
-
-    if (this.props.order === "1") {
-      sectionText = `How many applications does it take to land a job?
-                    Just one. 
-                    But let’s face it: many more will come first.
-
-                    Apply Yourself will keep your job applications organized
-                    and allow you to spend more time levelling up your skills!
-                  `;
-      sectionImage = "https://images.unsplash.com/photo-1456406644174-8ddd4cd52a06?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=ee742bee0f10b97a9d117d410d4887c8";
-      sectionBg = "grey-bg";
-    } else if (this.props.order === "2") {
-      sectionText = `<p>Forget which cover letter you sent where?
-
-                    <p>Track each step of the application process 
-                    for each job, from resumes to interviews 
-                    to job offers.</p>
-
-                    <p>Compare your prospects to choose which job suites you best!</p>
-                    `;
-      sectionImage = `https://images.unsplash.com/photo-1503551723145-6c040742065b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=cfcb36b45801576b92d7a537edb53ac1`;
       sectionBg = "white-bg";
-    }  
-      else if (this.props.order === "3") {
-      sectionText = `How’d that interview go?
-
-                    Mark your progress on each job prospect
-                    and find out where your weak points lie.
-
-                    Then hone your skills to reach maximum potential and land your next job!
-                    `;
-      sectionImage = "https://images.unsplash.com/photo-1507099985932-87a4520ed1d5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=6227a3e0898cc8a411a8e10cdee75db4";
-      sectionBg = "grey-bg";
-    } 
+    }
+    // <h2
+    //   dangerouslySetInnerHTML={{
+    //     __html: this.props.title
+    //   }}
+    // />
 
     return (
       <div className={`landing-section ${sectionBg}`}>
         <div className={`landing-image ${imageAlign}`}>
-          <img src={sectionImage} />
+          <img src={this.props.image} />
         </div>
         <div className={`landing-text ${textAlign}`}>
-            <h2>{this.props.title}</h2>
-            <div>{sectionText}</div>
+          <h2>{this.props.title}</h2>
+          <div>{this.props.content}</div>
         </div>
       </div>
     );
---
+++ src/components/page-components/landing-steps-container.js
@@ -0,0 +1,39 @@
+import React from "react";
+import LandingStep from "./landing-step";
+import "./landing-steps-container.css";
+
+const InnerHTML = require("dangerously-set-inner-html");
+
+export default function LandingSteps(props) {
+  return (
+    <div className={`landing-steps`}>
+      <h3>How It Works</h3>
+      <div className={`steps-group`}>
+        <LandingStep
+          title="Add Jobs"
+          img="https://images.unsplash.com/photo-1502758898459-10885c4d334c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=dfb01718cacade8007f7e86f0b385230"
+        >
+          Find a job of interest. Add it to your list. Include information such
+          as primary contact, desired skills and a job-specific cover letter.
+        </LandingStep>
+
+        <LandingStep
+          title="Keep Logs"
+          img="https://images.unsplash.com/photo-1511871893393-82e9c16b81e3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=f6ae3f201177b8677ecc928160d743f4"
+        >
+          Track your progress for each job by adding checkpoints such as sending
+          your resume, scheduling interviews, and following up with{" "}
+          <b>employers</b>
+        </LandingStep>
+
+        <LandingStep
+          title="View Stats"
+          img="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=6050a125f5034c4bf39301df9641f8f8"
+        >
+          Analyze statistics about your job search as a whole. Find weak points
+          to improve. Focus and hone your skills in order to come back stronger.
+        </LandingStep>
+      </div>
+    </div>
+  );
+}
