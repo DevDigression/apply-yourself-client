3c4ffe68f85f559b81edec9c76631a03cab3b1e6
Chris Rodgers <devdigression@gmail.com>
2018-02-05 21:37:58 -0500

    single job template responsive

--- src/components/pages/single-job.js
+++ src/components/pages/single-job.js
@@ -3,17 +3,15 @@ import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 
 import NavBar from "../navbar";
-import './single-job.css';
-
+import "./single-job.css";
 
 export function SingleJob(props) {
-
   let links = ["Job List", "Logout"];
   return (
     <div className="single-job">
       <NavBar links={links} />
       <div className="job-header">
-        <h3>Front End Developer</h3>
+        <h2>Front End Developer</h2>
         <h3>Automattic</h3>
         <button>See Job Posting</button>
         <img src="http://nvd3.org/examples/img/bullet.png" />
@@ -33,18 +31,38 @@ export function SingleJob(props) {
         </div>
         <div className="job-notes">
           <h4>Notes</h4>
-          <p>Remote work available - emphasize independent work in cover letter
-            Java + .NET are a plus - brush up on .NET
-            40 minute commute</p>
+          <p>
+            Remote work available - emphasize independent work in cover letter
+          </p>
+          <p>Java + .NET are a plus - brush up on .NET</p>
+          <p>40 minute commute</p>
         </div>
+      </div>
+      <div className="job-checkpoints">
+        <div className="checkpoints-header">
+          <h3>Checkpoints</h3>
+          <button>Add Checkpoint</button>
         </div>
-        <div className="job-checkpoints">
-          <ul>
-              <li>Checkpoint</li>
-              <li>Checkpoint</li>
-              <li>Checkpoint</li>
-          </ul>
+        <div className="checkpoints-list">
+          <div className="checkpoint">
+            <h5>1. Resume Sent</h5>
+            <p>Cover letter included:</p>
+            <p>Dear Hiring Manager ...</p>
+          </div>
+          <div className="checkpoint">
+            <h5>2. Phone Screen</h5>
+            <p>Simple algorithm questions</p>
+            <p>Asked about Node vs SQL</p>
+          </div>
+          <div className="checkpoint">
+            <h5>3. First Interview (Culture Fit)</h5>
+            <p>Got good feedback</p>
+            <p>
+              Questions: Why did you pick Automattic? I like working remotely
+            </p>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
