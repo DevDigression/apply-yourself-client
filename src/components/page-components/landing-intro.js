7d76283c66d77f201f7cc575de76ffc7b014feaf
Chris Rodgers <devdigression@gmail.com>
2018-02-06 14:03:54 -0500

    added background overlay to intro

--- src/components/page-components/landing-intro.js
+++ src/components/page-components/landing-intro.js
@@ -8,14 +8,16 @@ export default class LandingIntro extends React.Component {
   render() {
     return (
       <div className={`landing-intro`}>
-        <div className={`intro-heading`}>
-          {this.props.header}
-          <hr />
-        </div>
-        <div className={`intro-subheading`}>
-          <p>Organize your application process</p>
-          <p>and focus on landing the coding job</p>
-          <p>you’ve been looking for!</p>
+        <div className={`background-overlay`}>
+          <div className={`intro-heading`}>
+            {this.props.header}
+            <hr />
+          </div>
+          <div className={`intro-subheading`}>
+            <p>Organize your application process</p>
+            <p>and focus on landing the coding job</p>
+            <p>you’ve been looking for!</p>
+          </div>
         </div>
       </div>
     );
