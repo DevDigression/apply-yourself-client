9cb678d19d8a3b10dad873675b892989782d82ea
Sebastian Bastidas <sbastidasr@gmail.com>
2018-02-06 18:08:56 -0500

    roadmap to GET jobs from API

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -20,48 +20,23 @@ export default class Dashboard extends React.Component {
           <h5>Sort by: Date | Status</h5>
         </div>
         <div className="jobs-list">
+          this.props.jobs.map(job=>( // TODO: MAKE job-section A COMPONENT
           <div className="job-section">
             <div className="job-image">
               <img src="https://dcassetcdn.com/design_img/8324/18028/18028_236752_8324_thumbnail.jpg" />
             </div>
             <div className="job-text">
-              <h6>Senior React Engineer</h6>
+              <h6>{job.title}</h6>
               <p>Current Stage:</p>
               <p>7. Job Offer</p>
             </div>
             <div className="job-status">
               <p>01/20/2018</p>
               <p>Completion: 100%</p>
+              {job.notes}
             </div>
           </div>
-          <div className="job-section">
-            <div className="job-image">
-              <img src="https://www.cyberark.com/wp-content/uploads/2016/01/CyberArk-Logo-Intro.jpg" />
-            </div>
-            <div className="job-text">
-              <h6>Happiness Engineer</h6>
-              <p>Current Stage:</p>
-              <p>5. Technical Interview</p>
-            </div>
-            <div className="job-status">
-              <p>02/01/2018</p>
-              <p>Completion: 70%</p>
-            </div>
-          </div>
-          <div className="job-section">
-            <div className="job-image">
-              <img src="https://dcassetcdn.com/design_img/68129/25098/25098_1256774_68129_image.png" />
-            </div>
-            <div className="job-text">
-              <h6>iOS Engineer</h6>
-              <p>Current Stage:</p>
-              <p>Application Rejected</p>
-            </div>
-            <div className="job-status">
-              <p>02/03/2018</p>
-              <p>Completion: 15%</p>
-            </div>
-          </div>
+          ))
         </div>
         <div className="add-job">
           <button>Add Job</button>
@@ -71,12 +46,41 @@ export default class Dashboard extends React.Component {
   }
 }
 
-// const mapStateToProps = state => {
-//   const { currentUser } = state.auth;
-//   return {
-//     username: state.auth.currentUser.username,
-//     protectedData: state.protectedData.data
-//   };
-// };
+// <div className="job-section">
+//   <div className="job-image">
+//     <img src="https://www.cyberark.com/wp-content/uploads/2016/01/CyberArk-Logo-Intro.jpg" />
+//   </div>
+//   <div className="job-text">
+//     <h6>Happiness Engineer</h6>
+//     <p>Current Stage:</p>
+//     <p>5. Technical Interview</p>
+//   </div>
+//   <div className="job-status">
+//     <p>02/01/2018</p>
+//     <p>Completion: 70%</p>
+//   </div>
+// </div>
+// <div className="job-section">
+//   <div className="job-image">
+//     <img src="https://dcassetcdn.com/design_img/68129/25098/25098_1256774_68129_image.png" />
+//   </div>
+//   <div className="job-text">
+//     <h6>iOS Engineer</h6>
+//     <p>Current Stage:</p>
+//     <p>Application Rejected</p>
+//   </div>
+//   <div className="job-status">
+//     <p>02/03/2018</p>
+//     <p>Completion: 15%</p>
+//   </div>
+// </div>
+//
+
+const mapStateToProps = state => {
+  const { currentUser } = state.auth;
+  return {
+    jobs: state.protectedData.jobs
+  };
+};
 
 // export default requiresLogin()(connect(mapStateToProps)(Dashboard));
