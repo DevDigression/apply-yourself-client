c59379f1bb99ab1ffc7d3abbfc8ed6b4e765d7ca
Chris Rodgers <devdigression@gmail.com>
2018-02-11 17:04:24 -0500

    removed warnings

--- src/components/app.js
+++ src/components/app.js
@@ -46,7 +46,6 @@ export class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <HeaderBar />
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/stats" component={Stats} />
