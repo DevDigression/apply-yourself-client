a9a0bab249a10844ebe300d21b786e02ed6e7f2d
Chris Rodgers <devdigression@gmail.com>
2018-02-15 17:49:31 -0500

    removed warning

--- src/components/pages/single-job.js
+++ src/components/pages/single-job.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { fetchJobById } from "../../actions/protected-data";
 import { deleteJob } from "../../actions/protected-data";
 import NavBar from "../navbar";
