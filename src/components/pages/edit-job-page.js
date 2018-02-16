4f1024830df59b380c72c9c953ac4ef355454ba7
Sebastian Bastidas <sbastidasr@gmail.com>
2018-02-16 13:04:58 -0500

    unified form

--- src/components/pages/edit-job-page.js
+++ src/components/pages/edit-job-page.js
@@ -2,24 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import NavBar from "../navbar";
 import JobForm from "../page-components/job-form";
-import "./add-job-page.css";
+import "./add-job-page.css"; // TODO if the styles are the same, reuse them, dont create edit-job-page-css. but rename them to something that works for add + edit
 
 export class AddJobPage extends React.Component {
   componentDidMount() {
-    this.props.dispatch(DISPATCH_AN_ACTION_THAT_CLEANS_CURRENT_JOB);
-    // a clean currentJob would be:
-    // {title:"", company:"" checkpoints:[] .... and so on..  }
-    // NOT a null or empty object.
+    this.jobId = this.props.match.params.jobid;
+    if (this.jobId) {
+      this.props.dispatch(fetchJobById(this.props.match.params.jobid));
+    }
   }
 
   onSubmit(values) {
     values.keywords = values.keywords.split(",");
-    this.props.dispatch(addJob(values));
+    this.props.dispatch(editJob(values));
     return; // TODO props.history .... to redirect
   }
 
   render() {
-    let links = ["Stats", "Dashboard"];
+    let links = ["Stats", "Dashboard"]; // TODO do you need to change this?
     return (
       <div className="add-job-page">
         <NavBar links={links} />
