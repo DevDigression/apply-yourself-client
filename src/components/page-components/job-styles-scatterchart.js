fd0028071013009e4b1ead03289dcacf4ced06b7
Chris Rodgers <devdigression@gmail.com>
2018-03-02 08:46:07 -0500

    initial scatterchart setup

--- src/components/page-components/job-styles-scatterchart.js
+++ src/components/page-components/job-styles-scatterchart.js
@@ -8,18 +8,20 @@ jobsData(jobs, stages) {
               styles = ['startup', 'enterprise', 'nonprofit', 'contract'],
               shapes = ['circle', 'triangle-up', 'diamond', 'square']
 
-          for (var i = 0; i < jobs.length; i++) {
+          for (var i = 0; i < Object.keys(jobs).length; i++) {
               data.push({
                   key: styles[i],
                   values: []
               });
+
               for (var j = 0; j < stages; j++) {
                   data[i].values.push({
-                      x: Math.random()*10,
-                      y: Math.random()*5,
-                      size: Math.round(Math.random() * 100) / 100,
+                      x: jobs[data[i]],
+                      y: data[i].stage,
+                      // size: Math.round(Math.random() * 100) / 100,
                       shape: shapes[j-1]
                   });
+                  console.log(data[i]);
               }
           }
           return data;
@@ -28,13 +30,15 @@ jobsData(jobs, stages) {
 
 
     render() {
-      let jobStyles = Object.keys(this.props.styles);
+      let jobStyles = this.props.styles;
+      let jobStages = this.props.stages;
+      let totalJobs = this.props.totalJobs;
       console.log(jobStyles)
       return (
         <div>
           <NVD3Chart
             type="scatterChart"
-            datum={this.jobsData(jobStyles, 10)}
+            datum={this.jobsData(jobStyles, totalJobs)}
             containerStyle={{ width: 500, height: 500 }}
           />
         </div>
